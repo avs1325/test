dbfe73ed42007c3d2726e444ec036e9846921daa
avs1325 <61194716+avs1325@users.noreply.github.com>
2020-09-01 11:48:34 +0530

    Update eq1.js

--- eq1.js
+++ eq1.js
@@ -9,33 +9,33 @@ var calculateButton, calculatePressed;
 
 
 function setup (){
-    createCanvas(200, 500);
+    createCanvas(250, 600);
     vInput = createInput("Final Velocity");
     uInput = createInput("Initial Velocity");
     tInput = createInput("Time");
     calculateButton = createButton("CALCULATE");
 
-    vInput.position(0, 100);
-    uInput.position(0, 125);
-    tInput.position(0, 150);
-    calculateButton.position(0, 200);
+    vInput.position(80, 300);
+    uInput.position(80, 350);
+    tInput.position(80, 400);
+    calculateButton.position(8, 500);
 
 
     vInput.size(100, 50);
     uInput.size(100, 50);
     tInput.size(100, 50);
 
-    calculateButton.size(200,100);
+    calculateButton.size(250,100);
     calculatePressed = "no";
 }
 
 function draw () {
     background(0, 255, 255);
-    textSize(10)
+    textSize(15)
 
-    text("Speed should be entered in m/s.", 0 , 45)
-    text("Time should be entered in s.", 0 , 90)
-    text("Acceleration will be returned in m/s/s.", 0 , 135)
+    text("Speed should be entered in m/s.", 0 , 30)
+    text("Time should be entered in s.", 0 , 60)
+    text("Acceleration will be returned in m/s/s.", 0 , 90)
 
     calculateButton.mousePressed(() => {
         v = vInput.value();
@@ -45,18 +45,19 @@ function draw () {
     })
     if (calculatePressed == "yes"){
         if (isNaN(v) || isNaN(u) || isNaN(t)) {
-            text("Please enter numerical values", 10 , 225);
+            text("Please enter numerical values", 0 , 150);
         }
         if (!isNaN(v) && !isNaN(u) && !isNaN(t)) {
             a = (v-u)/t;
             console.log(a)
-            text("If you want to enter different data, enter it and press calculate", 10 , 225);
-            text("Else, your current data is:", 10 , 270);
-            text("Final velocity  : " + v + " m/s", 10 , 315);
-            text("Initial velocity: " + u + " m/s", 10 , 360);
-            text("Time: " + t + "s", 10 , 405);
-            text("The equation being used is (v-u)/t", 10 , 450);
-            text("The answer is " + a + " m/s/s", 10 , 495);
+            text("If you want to enter different data,", 0, 120);
+            text("enter it and press calculate", 0, 135)
+            text("Else, your current data is:", 0, 155);
+            text("Final velocity  : " + v + " m/s", 0, 175);
+            text("Initial velocity: " + u + " m/s", 0, 190);
+            text("Time: " + t + "s", 0, 205);
+            text("The equation being used is (v-u)/t", 0, 225);
+            text("The answer is " + a + " m/s/s", 0, 245);
         }
     }
 }
